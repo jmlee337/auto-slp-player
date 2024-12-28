8c789ecb6bf8c75c50f042eb6fbd6270a38d7650
Nicolet <jmlee337@gmail.com>
2024-12-27 18:59:13 -0600

    fix regression on input mute and enabling
    also remove `checkObsSetup` and `magic-active-win`
    
    #27

--- src/main/obs.ts
+++ src/main/obs.ts
@@ -1,6 +1,5 @@
 import OBSWebSocket, { RequestBatchRequest } from 'obs-websocket-js';
 import { BrowserWindow } from 'electron';
-import { getOpenWindows } from 'magic-active-win';
 import {
   AvailableSet,
   OBSConnectionStatus,
@@ -44,9 +43,9 @@ export default class OBSConnection {
 
   private pidToPort: Map<number, number>;
 
-  private portToUuid: Map<number, string>;
+  private portToInputName: Map<number, string>;
 
-  private sceneNameToUuidToSceneItemId: Map<string, Map<string, number>>;
+  private sceneNameToInputNameToSceneItemId: Map<string, Map<string, number>>;
 
   private streamingState: string;
 
@@ -73,8 +72,8 @@ export default class OBSConnection {
     this.maxDolphins = 0;
     this.obsWebSocket = null;
     this.pidToPort = new Map();
-    this.portToUuid = new Map();
-    this.sceneNameToUuidToSceneItemId = new Map();
+    this.portToInputName = new Map();
+    this.sceneNameToInputNameToSceneItemId = new Map();
     this.streamingState = '';
   }
 
@@ -101,238 +100,6 @@ export default class OBSConnection {
   private async getPrefix() {
     const dolphinVersion = await this.dolphinVersionPromise;
     return `Faster Melee - Slippi (${dolphinVersion}) - Playback | `;
-  }
-
-  private async checkObsSetup() {
-    if (
-      this.obsWebSocket === null ||
-      this.connectionStatus === OBSConnectionStatus.OBS_NOT_CONNECTED ||
-      !this.dolphinVersionPromise ||
-      !(process.platform === 'win32' || process.platform === 'darwin')
-    ) {
-      return;
-    }
-    if (this.dolphinPorts.length < this.maxDolphins) {
-      this.setConnectionStatus(
-        OBSConnectionStatus.OBS_NOT_SETUP,
-        `${this.dolphinPorts.length} dolphins open. Expected ${this.maxDolphins}.`,
-      );
-      return;
-    }
-
-    const portToUuid = new Map<number, string>();
-    let inputKind = '';
-    if (process.platform === 'win32') {
-      inputKind = 'game_capture';
-    } else if (process.platform === 'darwin') {
-      inputKind = 'screen_capture';
-    }
-    const truncPorts = this.dolphinPorts.slice(0, this.maxDolphins);
-    const { inputs } = await this.obsWebSocket.call('GetInputList', {
-      inputKind,
-    });
-    if (process.platform === 'win32') {
-      const prefix = await this.getPrefix();
-      const expectedPortStrs = new Set(
-        truncPorts.map((port) => port.toString()),
-      );
-      await Promise.all(
-        inputs.map(async (input) => {
-          const { inputUuid } = input as { inputUuid: string };
-          // {"capture_audio":true,"capture_mode":"window","priority":1,"window":"Faster Melee - Slippi (3.4.2) - Playback | 51441:wxWindowNR:Slippi Dolphin.exe"}
-          const { inputSettings } = await this.obsWebSocket!.call(
-            'GetInputSettings',
-            { inputUuid },
-          );
-          if (
-            inputSettings.capture_audio &&
-            inputSettings.capture_mode === 'window' &&
-            inputSettings.priority === 1
-          ) {
-            const { window } = inputSettings as { window: string };
-            const endI = window.indexOf(':');
-            if (window.startsWith(prefix) && endI > prefix.length) {
-              const portStr = window.slice(endI - 5, endI);
-              if (expectedPortStrs.has(portStr)) {
-                expectedPortStrs.delete(portStr);
-                portToUuid.set(Number.parseInt(portStr, 10), inputUuid);
-              }
-            }
-          }
-        }),
-      );
-      if (expectedPortStrs.size !== 0) {
-        const notFound = Array.from(expectedPortStrs.keys()).sort().join(', ');
-        this.setConnectionStatus(
-          OBSConnectionStatus.OBS_NOT_SETUP,
-          `Inputs not found for dolphin(s): ${notFound}. Check the "Window", "Window match priority" ("Window title must match"), and "Capture Audio" (enabled) settings on your Game Capture inputs.`,
-        );
-        return;
-      }
-    } else if (process.platform === 'darwin') {
-      const expectedPorts = new Set(truncPorts);
-      const expectedPids = new Set(
-        Array.from(this.pidToPort.entries())
-          .filter(([, port]) => expectedPorts.has(port))
-          .map(([pid]) => pid),
-      );
-      await Promise.all(
-        inputs.map(async (input) => {
-          const { inputUuid } = input as { inputUuid: string };
-          // {"application":"com.project-slippi.dolphin","show_cursor":false,"show_empty_names":false,"show_hidden_windows":false,"type":1,"window":15540}
-          const { inputSettings } = await this.obsWebSocket!.call(
-            'GetInputSettings',
-            { inputUuid },
-          );
-          const windowToPid = new Map<number, number>();
-          (
-            await getOpenWindows({
-              accessibilityPermission: false,
-              screenRecordingPermission: false,
-            })
-          ).forEach((window) => {
-            const pid = window.owner.processId;
-            if (this.pidToPort.has(pid)) {
-              windowToPid.set(window.id, pid);
-            }
-          });
-          const pid = windowToPid.get(inputSettings.window as number);
-          if (inputSettings.type === 1 && pid) {
-            if (expectedPids.has(pid)) {
-              expectedPids.delete(pid);
-              portToUuid.set(this.pidToPort.get(pid)!, inputUuid);
-            }
-          }
-        }),
-      );
-      if (expectedPids.size !== 0) {
-        const notFound = Array.from(expectedPids.keys())
-          .map((pid) => this.pidToPort.get(pid)!)
-          .sort((a, b) => a - b)
-          .join(', ');
-        this.setConnectionStatus(
-          OBSConnectionStatus.OBS_NOT_SETUP,
-          `Inputs not found for dolphin(s): ${notFound}. Check the "Window" and "Method" ("Window Capture") settings on your macOS Screen Capture inputs.`,
-        );
-        return;
-      }
-    }
-    this.portToUuid = portToUuid;
-
-    /**
-     * [{"sceneIndex":0,"sceneName":"quad 4","sceneUuid":"f843d4e5-f9ec-44da-9a8c-0a9ffe52b440"},
-     *  {"sceneIndex":1,"sceneName":"quad 3","sceneUuid":"87769989-619c-4c46-b42e-362a43452250"},
-     *  {"sceneIndex":2,"sceneName":"quad 2","sceneUuid":"b07436ab-0eda-47be-99d8-ba33f420f4f4"},
-     *  {"sceneIndex":3,"sceneName":"quad 1","sceneUuid":"7eb22cbe-b737-4de2-b8a5-0ce61cf591ad"}]
-     */
-    const { scenes } = await this.obsWebSocket.call('GetSceneList');
-    const sceneNames = new Set(scenes.map((scene) => scene.sceneName));
-    for (let i = 0; i <= this.maxDolphins; i += 1) {
-      if (i !== 2) {
-        const sceneName = `quad ${i}`;
-        if (!sceneNames.has(sceneName)) {
-          this.setConnectionStatus(
-            OBSConnectionStatus.OBS_NOT_SETUP,
-            `Scene "${sceneName}" not found.`,
-          );
-          return;
-        }
-        if (i !== 0) {
-          const expectedUuids = new Set(portToUuid.values());
-          const uuidToSceneItemId = new Map<string, number>();
-          /**
-           * [{"inputKind":"browser_source","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":false,"sceneItemId":10,"sceneItemIndex":0,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":0,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":0,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":492,"positionX":880,"positionY":0,"rotation":0,"scaleX":1,"scaleY":1,"sourceHeight":492,"sourceWidth":400,"width":400},"sourceName":"Browser","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"c8d1a976-b255-4a77-a193-b12463546c83"},
-           *  {"inputKind":"game_capture","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":true,"sceneItemId":15,"sceneItemIndex":1,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":1,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":1,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":0,"positionX":660,"positionY":540,"rotation":0,"scaleX":1.03125,"scaleY":1.0305343866348267,"sourceHeight":0,"sourceWidth":0,"width":0},"sourceName":"dolphin 3","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"8e1a48ce-1b69-46e7-98ba-fe3bf7fd597f"},
-           *  {"inputKind":"game_capture","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":true,"sceneItemId":14,"sceneItemIndex":2,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":0,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":0,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":0,"positionX":0,"positionY":540,"rotation":0,"scaleX":1.03125,"scaleY":1.0305343866348267,"sourceHeight":0,"sourceWidth":0,"width":0},"sourceName":"dolphin 2","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"90a2c347-ad1e-4ce8-b68e-286adc96696d"},
-           *  {"inputKind":"game_capture","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":true,"sceneItemId":13,"sceneItemIndex":3,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":0,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":0,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":0,"positionX":660,"positionY":0,"rotation":0,"scaleX":1.03125,"scaleY":1.0305343866348267,"sourceHeight":0,"sourceWidth":0,"width":0},"sourceName":"dolphin 1","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"472771e8-8a4d-4fe7-8974-6a0e0c660941"},
-           *  {"inputKind":"game_capture","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":true,"sceneItemId":12,"sceneItemIndex":4,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":0,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":0,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":0,"positionX":0,"positionY":0,"rotation":0,"scaleX":1.03125,"scaleY":1.0305343866348267,"sourceHeight":0,"sourceWidth":0,"width":0},"sourceName":"dolphin 0","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"2aab46b4-e356-4708-8165-33bd139a9f95"}]
-           */
-          const { sceneItems } = await this.obsWebSocket.call(
-            'GetSceneItemList',
-            { sceneName },
-          );
-          sceneItems
-            .filter((sceneItem) => sceneItem.inputKind === inputKind)
-            .forEach(async (sceneItem) => {
-              const { sourceUuid } = sceneItem as { sourceUuid: string };
-              if (expectedUuids.has(sourceUuid)) {
-                expectedUuids.delete(sourceUuid);
-                const { sceneItemId } = sceneItem as { sceneItemId: number };
-                uuidToSceneItemId.set(sourceUuid, sceneItemId);
-              }
-            });
-          if (expectedUuids.size !== 0) {
-            this.setConnectionStatus(
-              OBSConnectionStatus.OBS_NOT_SETUP,
-              `Scene: "quad ${i}" doesn't contain all dolphin game inputs.`,
-            );
-            return;
-          }
-          this.sceneNameToUuidToSceneItemId.set(sceneName, uuidToSceneItemId);
-        }
-      }
-    }
-    if (this.maxDolphins > 1) {
-      const expectedScenes = [{ sceneName: 'quad 2 12', indices: [0, 1] }];
-      if (this.maxDolphins > 2) {
-        expectedScenes.push(
-          { sceneName: 'quad 2 13', indices: [0, 2] },
-          { sceneName: 'quad 2 23', indices: [1, 2] },
-        );
-        if (this.maxDolphins > 3) {
-          expectedScenes.push(
-            { sceneName: 'quad 2 14', indices: [0, 3] },
-            { sceneName: 'quad 2 24', indices: [1, 3] },
-            { sceneName: 'quad 2 34', indices: [2, 3] },
-          );
-        }
-      }
-      for (const { sceneName, indices } of expectedScenes) {
-        if (!sceneNames.has(sceneName)) {
-          this.setConnectionStatus(
-            OBSConnectionStatus.OBS_NOT_SETUP,
-            `Scene "${sceneName}" not found.`,
-          );
-          return;
-        }
-        const expectedPorts = indices.map((index) => this.dolphinPorts[index]);
-        const expectedUuids = new Set(
-          expectedPorts.map((port) => portToUuid.get(port)!),
-        );
-        const uuidToSceneItemId = new Map<string, number>();
-        /**
-         * [{"inputKind":"browser_source","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":false,"sceneItemId":10,"sceneItemIndex":0,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":0,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":0,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":492,"positionX":880,"positionY":0,"rotation":0,"scaleX":1,"scaleY":1,"sourceHeight":492,"sourceWidth":400,"width":400},"sourceName":"Browser","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"c8d1a976-b255-4a77-a193-b12463546c83"},
-         *  {"inputKind":"game_capture","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":true,"sceneItemId":15,"sceneItemIndex":1,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":1,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":1,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":0,"positionX":660,"positionY":540,"rotation":0,"scaleX":1.03125,"scaleY":1.0305343866348267,"sourceHeight":0,"sourceWidth":0,"width":0},"sourceName":"dolphin 3","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"8e1a48ce-1b69-46e7-98ba-fe3bf7fd597f"},
-         *  {"inputKind":"game_capture","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":true,"sceneItemId":14,"sceneItemIndex":2,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":0,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":0,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":0,"positionX":0,"positionY":540,"rotation":0,"scaleX":1.03125,"scaleY":1.0305343866348267,"sourceHeight":0,"sourceWidth":0,"width":0},"sourceName":"dolphin 2","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"90a2c347-ad1e-4ce8-b68e-286adc96696d"},
-         *  {"inputKind":"game_capture","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":true,"sceneItemId":13,"sceneItemIndex":3,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":0,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":0,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":0,"positionX":660,"positionY":0,"rotation":0,"scaleX":1.03125,"scaleY":1.0305343866348267,"sourceHeight":0,"sourceWidth":0,"width":0},"sourceName":"dolphin 1","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"472771e8-8a4d-4fe7-8974-6a0e0c660941"},
-         *  {"inputKind":"game_capture","isGroup":null,"sceneItemBlendMode":"OBS_BLEND_NORMAL","sceneItemEnabled":true,"sceneItemId":12,"sceneItemIndex":4,"sceneItemLocked":true,"sceneItemTransform":{"alignment":5,"boundsAlignment":0,"boundsHeight":0,"boundsType":"OBS_BOUNDS_NONE","boundsWidth":0,"cropBottom":0,"cropLeft":0,"cropRight":0,"cropTop":0,"height":0,"positionX":0,"positionY":0,"rotation":0,"scaleX":1.03125,"scaleY":1.0305343866348267,"sourceHeight":0,"sourceWidth":0,"width":0},"sourceName":"dolphin 0","sourceType":"OBS_SOURCE_TYPE_INPUT","sourceUuid":"2aab46b4-e356-4708-8165-33bd139a9f95"}]
-         */
-        const { sceneItems } = await this.obsWebSocket.call(
-          'GetSceneItemList',
-          { sceneName },
-        );
-        sceneItems
-          .filter((sceneItem) => sceneItem.inputKind === inputKind)
-          .forEach(async (sceneItem) => {
-            const { sourceUuid } = sceneItem as { sourceUuid: string };
-            if (expectedUuids.has(sourceUuid)) {
-              expectedUuids.delete(sourceUuid);
-              const { sceneItemId } = sceneItem as { sceneItemId: number };
-              uuidToSceneItemId.set(sourceUuid, sceneItemId);
-            }
-          });
-        if (expectedUuids.size !== 0) {
-          const expectedPortsStr = expectedPorts.join(', ');
-          this.setConnectionStatus(
-            OBSConnectionStatus.OBS_NOT_SETUP,
-            `Scene: "${sceneName}" should contain game inputs for dolphins: ${expectedPortsStr}.`,
-          );
-          return;
-        }
-        this.sceneNameToUuidToSceneItemId.set(sceneName, uuidToSceneItemId);
-      }
-    }
-    this.setConnectionStatus(OBSConnectionStatus.READY);
   }
 
   setMaxDolphins(maxDophins: number) {
@@ -529,19 +296,18 @@ export default class OBSConnection {
             if (!itemName.startsWith(startsWith)) {
               return false;
             }
-            if (
-              !itemName
-                .slice(startsWith.length)
-                .match('^[0-9][0-9][0-9][0-9][0-9]$')
-            ) {
+            if (!itemName.slice(-5).match('[0-9][0-9][0-9][0-9][0-9]')) {
               return false;
             }
             return true;
           })
-          .map((propertyItem) => propertyItem.itemValue as number);
+          .map((propertyItem) => ({
+            port: parseInt((propertyItem.itemName as string).slice(-5), 10),
+            window: propertyItem.itemValue as number,
+          }));
       };
       let retries = 0;
-      let windows: number[] = [];
+      let windows: { port: number; window: number }[] = [];
       while (windows.length < this.maxDolphins) {
         if (retries === 4) {
           this.setConnectionStatus(
@@ -556,11 +322,13 @@ export default class OBSConnection {
         windows = await getWindows(this.obsWebSocket);
         retries += 1;
       }
+      this.portToInputName.clear();
       for (let i = 0; i < this.maxDolphins; i += 1) {
         await this.obsWebSocket.call('SetInputSettings', {
           inputName: expectedInputNames[i],
-          inputSettings: { window: windows[i] },
+          inputSettings: { window: windows[i].window },
         });
+        this.portToInputName.set(windows[i].port, expectedInputNames[i]);
       }
     } else {
       // windows todo
@@ -630,6 +398,11 @@ export default class OBSConnection {
         );
         sourceNameToSceneItemId.set(sourceName, sceneItemId);
       }
+      this.sceneNameToInputNameToSceneItemId.set(
+        sceneName,
+        sourceNameToSceneItemId,
+      );
+
       for (let i = 0; i < expectedSourceNames.length; i += 1) {
         const sceneItemId = sourceNameToSceneItemId.get(
           expectedSourceNames[i],
@@ -743,12 +516,12 @@ export default class OBSConnection {
     });
     if (ports.length !== 0) {
       this.dolphinPorts.forEach((port) => {
-        const uuid = this.portToUuid.get(port);
-        if (uuid) {
+        const inputName = this.portToInputName.get(port);
+        if (inputName) {
           requests.push({
             requestType: 'SetInputMute',
             requestData: {
-              inputUuid: uuid,
+              inputName,
               inputMuted: port !== ports[0],
             },
           });
@@ -757,11 +530,11 @@ export default class OBSConnection {
       if (!isQuad2SpecialCase) {
         const enabledPorts = new Set(ports);
         this.dolphinPorts.forEach((port) => {
-          const uuid = this.portToUuid.get(port);
-          if (uuid) {
-            const sceneItemId = this.sceneNameToUuidToSceneItemId
+          const inputName = this.portToInputName.get(port);
+          if (inputName) {
+            const sceneItemId = this.sceneNameToInputNameToSceneItemId
               .get(sceneName)
-              ?.get(uuid);
+              ?.get(inputName);
             if (sceneItemId) {
               requests.push({
                 requestType: 'SetSceneItemEnabled',
